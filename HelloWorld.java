1ea3b1685c8ff08db364905dc49f26b7f9e6fff8
Oyvind Selmer <oyvind.selmer@gmail.com>
2010-09-12 17:58:22 +0200

    Changed the layout in the src file
    Made it look cleaner, with the right code block conventions etc.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,17 @@
 class HelloWorld {
-public static void main (String[] args) {
-	System.out.println("Andre er en FISK");
-	System.out.println("en tunfisk");
+	public String Sjekk() {
+		return "bare tull";
+	}
+
+	public static void main (String[] args) {
+		System.out.println("Andre er en FISK");
+		System.out.println("en tunfisk");
 
         String[] idioter = {"Selmer", "Paal", "Jinxen", "Greve"};
         System.out.println("Følgende personer er idioter: ");
-        for(int i=0;i<idioter.length;i++) {
-              System.out.println("Idiot nr"+(i+1) +" er: "+idioter[i]+"");
-	}
-
-	System.out.println("That's it, that's all!");
-}//main
+	    for(int i=0;i<idioter.length;i++) {
+			System.out.println("Idiot nr"+(i+1) +" er: "+idioter[i]+"");
+		}
+		System.out.println("That's it, that's all!");
+	}//main
 }//HelloWorld.java
