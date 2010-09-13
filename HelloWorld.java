230f6cbe9684d90c799ac2fccc6ba7f4f59f62fc
unknown <Nelich@.(none)>
2010-09-13 18:29:36 +0200

    YO!

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Andre er en FISK");
 		System.out.println("en tunfisk");
-
+// Hei alle sammen !
         String[] idioter = {"Selmer", "Paal", "Jinxen", "Greve"};
         System.out.println("Følgende personer er idioter: ");
 	    for(int i=0;i<idioter.length;i++) {
